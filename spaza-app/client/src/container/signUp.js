2e11b2ded18762c52820ee2093f616cd4f812848
maggiemoteno2 <motenomaggie@gmail.com>
2020-08-25 14:21:44 +0200

    added function that submit the users crendentials

--- spaza-app/client/src/container/signUp.js
+++ spaza-app/client/src/container/signUp.js
@@ -1,38 +1,57 @@
-import React from 'react'
+import logIn from './logIn'
+import { Route, Switch, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {register} from './../redux/actions'
 
-export default function signUp() {
+ const SignUp=()=> {
+  const [credentials, setCredentials] = useState({ password: '', email: '', name: '' })
+  const dispatch = useDispatch()
+
+  const addCredentials = () => {
+    console.log(credentials)
+    dispatch(register(credentials))
+  }
+
   return (
     <div>
       <div class="signup-form">
-        <form action="/examples/actions/confirmation.php" method="post">
+        <form>
           <h2>Register</h2>
-          <p class="hint-text">Create your account. It's free and only takes a minute.</p>
+          <p class="hint-text">Create your account.</p>
           <div class="form-group">
 <div class="row">
-              <div class="col"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"/></div>
-                <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"/></div>
+              <div class="col"><input type="text" class="form-control" name="name" placeholder="Name" onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })}
+                value={credentials.name} required="required"/></div>
+         
 </div>
           </div>
           <div class="form-group">
-            <input type="email" class="form-control" name="email" placeholder="Email" required="required"/>
+            <input type="email" class="form-control" name="email" onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })}
+              value={credentials.email} placeholder="Email" required="required" />
           </div>
           <div class="form-group">
-            <input type="password" class="form-control" name="password" placeholder="Password" required="required"/>
-          </div>
-          <div class="form-group">
-            <input type="password" class="form-control" name="confirm_password" placeholder="Confirm Password" required="required"/>
+            <input type="password" class="form-control" onChange={(e) => setCredentials({ ...credentials, [e.target.name]: e.target.value })}
+              value={credentials.password} name="password" placeholder="Password" required="required"/>
           </div>
           <div class="form-group">
             <label class="form-check-label"><input type="checkbox" required="required"/> I accept the <a href="#">Terms of Use</a> &amp; <a href="#">Privacy Policy</a></label>
           </div>
           <div class="form-group">
-            <button type="submit" class="btn btn-success btn-lg btn-block">Register Now</button>
+            <button type="submit" onClick={() => addCredentials()} class="btn btn-success btn-lg btn-block">Register Now</button>
           </div>
           </form>
-        <div class="text-center">Already have an account? <a href="http://localhost:3000">Sign in</a></div>
-
+        <Router>
+        <div class="text-center">Already have an account?  <NavLink to={logIn}>Sign in</NavLink></div>
+        
+        </Router>
       </div>
       
     </div>
   )
 }
+
+
+
+export default SignUp;
