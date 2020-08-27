03924747221116e832fc98a8fb7d325a01eaef21
maggiemoteno2 <motenomaggie@gmail.com>
2020-08-27 13:16:31 +0200

    added total amount code

--- spaza-app/client/src/container/CartList.js
+++ spaza-app/client/src/container/CartList.js
@@ -4,14 +4,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CartList () {
-  const shoppingCart = useSelector(state => state.shopping.carts
-    ,state=> state.shopping.amount)
+  const shoppingCart = useSelector(state => state.shopping.carts)
+  const totalAmount = useSelector(state => state.shopping.amount)
 
   return (
     <div>
       <div>
         <h1> Cart List</h1>
   {shoppingCart.map(cartItem => <div>{cartItem.name} {cartItem.price}</div>)}
+  total amount :{totalAmount}
       </div>
     </div>
   )
